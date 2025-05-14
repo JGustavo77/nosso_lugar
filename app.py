d70e26aaa2bbebe735d095a6c07277a216d81001
JGustavo <137926999+JGustavo77@users.noreply.github.com>
2025-05-14 01:33:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
+from views import views_blueprint
 
 app = Flask(__name__)
-
-# Importa as rotas
-import views
+app.register_blueprint(views_blueprint)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host="0.0.0.0", port=port)
