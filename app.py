ce157a9197b6dfa92ca336cf575cf437131ac08d
JGustavo <137926999+JGustavo77@users.noreply.github.com>
2025-05-21 23:20:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-import os
 from flask import Flask
-from views import views_blueprint
+import views  # importa o arquivo, NÃO uma variável ou objeto
 
 app = Flask(__name__)
-app.register_blueprint(views_blueprint)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True)
