cc456b350a25c53bcd2c207a4629179dc11ccda6
JGustavo <137926999+JGustavo77@users.noreply.github.com>
2025-05-21 23:38:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-import views  # importa as rotas
+import views  # só importa o módulo
 
 app = Flask(__name__)
 
 import os
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
